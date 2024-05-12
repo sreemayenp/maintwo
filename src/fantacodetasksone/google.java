5711fc1dfbfc28bd65481fba1c662c748dd73fb9
sreemayenp <sreemaye63@gmail.com>
2024-05-12 10:40:07 +0530

    firstcommit.project

---
+++ src/fantacodetasksone/google.java
@@ -0,0 +1,10 @@
+package fantacodetasksone;
+
+public class google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module fantacodetasks {
+	requires org.seleniumhq.selenium.api;
+}
